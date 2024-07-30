cd253280fd825b6e07d5d2f9a0a856986ec5a44d
cam <cambo9p.11@gmail.com>
2024-07-30 20:29:01 +1200

    added parsing for more options and better error handling

--- src/main.c
+++ src/main.c
@@ -5,13 +5,15 @@
 #include "../include/searchfile.h"
 
 typedef enum{
-    NO_OPT,
+    RECURSIVE_OPT,
+    LINE_NUM_OPT,
+    COUNT_OPT,
 } Options_t;
 
 // takes array with filename and 
 void cgrep_find_in_file(int inputCount, char **input) {
-    char *filename = input[0];
-    char *pattern = input[1];
+    char *filename = input[1];
+    char *pattern = input[0];
     printf("searching for '%s' in %s\n", pattern, filename);
     
     cgrep_search(filename, pattern);
@@ -27,26 +29,38 @@ void cgrep_read_from_stdin() {
 
 int main(int argc, char *argv[]) {
     int opt;
+    Options_t *options;
     
-    while ((opt = getopt(argc, argv, "r:")) != -1) {
+    int i = 0;
+    while ((opt = getopt(argc, argv, "rcn")) != -1) {
         switch (opt) {
             case 'r':
                 // TODO
-                printf("option %s\n",optarg);
+                //printf("option %s\n",optarg); // this is when 'r:' is used
+                options[i] = RECURSIVE_OPT;
+                break;
+            case 'c':
+                options[i] = COUNT_OPT;
+                break;
+            case 'n':
+                options[i] = LINE_NUM_OPT;
                 break;
             default:
                 break;
         }
+        i++;
     }
     // TODO: check if we need to get from stdin
 
     int numArgs = argc - optind;
     char **args = &argv[optind++];
 
-    if (numArgs > 2) {
-        printf("error");
+    if (numArgs != 2) {
+        fprintf(stderr, "Useage: %s [-rcn] [file...]\n", argv[0]);
         exit(-1);
     }
+
+    // TODO multi opt parsing
 
     cgrep_find_in_file(numArgs, args);
 
