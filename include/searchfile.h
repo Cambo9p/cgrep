e6d7fd33f8888e0cf9ba8c813d691ac7ac38f7af
cam <cambo9p.11@gmail.com>
2024-08-09 19:49:17 +1200

    added linear searct function as public so dir can call it

--- include/searchfile.h
+++ include/searchfile.h
@@ -1,7 +1,10 @@
 #ifndef SEARCH_H
 #define SEARCH_H
+#include "args.h"
+#include "formatted_ans.h"
 
 // searches for the pattern in file TODO add options passed into here
 void cgrep_search_file(char *filename, char *pattern);
+void cgrep_linear_search(grep_args_t *args);
 
 #endif
